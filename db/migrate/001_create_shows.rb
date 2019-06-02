48cc1762d2a89895114e10bd538e9e0bcded3f60
Kat B <katreesebarnes@gmail.com>
2019-06-02 20:38:11 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,5 +1,9 @@
 class CreateShows < ActiveRecord::Migration[5.1]
   def change :shows do |t|
-    
-    
+    t.string :name 
+    t.string :network 
+    t.string :day 
+    t.integer :rating 
   end
+ end
+end
